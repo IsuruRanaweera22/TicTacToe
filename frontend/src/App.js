85cdd86212b1a655173962074a02ac74a95a566d
Dinal Fernando <56021139+Dinal-Fernando@users.noreply.github.com>
2024-10-16 14:08:21 +0530

    added all

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Sample App</h1>
+    </div>
+  );
+}
+
+export default App;
